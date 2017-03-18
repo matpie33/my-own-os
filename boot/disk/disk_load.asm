9a94a2d8e5d8a417f1acfaf92fd8237b80da655f
Mateusz <Spaaw@poczta.fm>
2017-03-18 10:06:55 +0100

    Add message about failed operation in disk load incluing error code.

--- boot/disk/disk_load.asm
+++ boot/disk/disk_load.asm
@@ -1,5 +1,5 @@
 ; load DH sectors to ES:BX from drive DL
-disk_load :
+disk_load:
 	push dx ; Store DX on stack so later we can recall
 	; how many sectors were request to be read ,
 	; even if it is altered in the meantime
@@ -14,15 +14,22 @@ disk_load :
 	jc disk_error ; Jump if error ( i.e. carry flag set )
 	pop dx ; Restore DX from the stack
 	cmp dh, al ; if AL ( sectors read ) != DH ( sectors expected )
-	jne disk_error ; display error message
-	mov bx, SUCCESS
-	call print
+	jne sector_error ; display error message
 	ret
 	
-disk_error :
+disk_error:
 	mov bx, DISK_ERROR_MSG
+	call print
+	mov dh, ah ; ah = error code, dl = disk drive that dropped the error
+    call print_hex ; check out the code at http://stanislavs.org/helppc/int_13-1.html
+	jmp $
+
+
+sector_error:
+	mov bx, SECTOR_ERROR_MSG
 	call print
 	jmp $
 	; Variables
 DISK_ERROR_MSG db "Disk read error!", 0
 SUCCESS db "Successfully read from disk!",0
+SECTOR_ERROR_MSG db "Sectors error", 0
