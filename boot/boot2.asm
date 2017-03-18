5af834870da59538286041fe7c5846176f8d06d1
Mateusz <Spaaw@poczta.fm>
2017-03-18 11:25:02 +0100

    Working solution: getting vesa information.

--- boot/boot2.asm
+++ boot/boot2.asm
@@ -5,24 +5,30 @@ mov [BOOT_DRIVE], dl ; BIOS stores our boot drive in DL , so it 's
 
 mov bp, 0x9000 ; Set -up the stack.
 mov sp, bp
-mov bx, MSG_REAL_MODE
-call print
+
+
+ 
+
+
+
+
+
 [bits 16]
-load2ndStage:
-mov bx, KERNEL_OFFSET   ; Set -up parameters for our disk_load routine , so
-mov cl, 0x02
-mov dh, 31				; that we load the first 15 sectors ( excluding
-mov dl, [ BOOT_DRIVE ]  ; the boot sector ) from the boot disk ( i.e. our
-call disk_load 			; kernel code ) to address KERNEL_OFFSET
-
-call switch_to_pm
-BEGIN_PM:
+; VESA setting mode code
 
 
 
+push es					; some VESA BIOSes destroy ES, or so I read
+	mov ax, 0x4F00				; get VBE BIOS info
+	mov di, vbe_info_block
+	int 0x10
+	pop es
 
+mov dx, vbe_info_block.video_memory
+call print
 
 jmp $
+
 
 %include "boot/real_mode/print.asm"
 %include "boot/real_mode/print_hex.asm"
@@ -31,7 +37,6 @@ jmp $
 %include "boot/disk/disk_load.asm"
 %include "boot/pm/gdt.asm"
 %include "boot/pm/print_string_pm.asm"
-%include "boot/pm/switch_to_pm.asm"
 %include "boot/pm/clear_screen.asm"
 
 MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
@@ -39,4 +44,7 @@ MSG_WAIT db "Now waiting...", 0
 MSG_PROT_MODE db "Successfully landed in 32- bit Protected Mode.", 0
 MSG_LOAD_KERNEL db "Loading kernel into memory.", 0
 BOOT_DRIVE db 0
+vbe_info_block:
+	.signature		db "VBE2"	; indicate support for VBE 2.0+
+	.table_data:	times 512-4 db 0	; reserve space for the table below
 
