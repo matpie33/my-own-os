e6ec7ac192500ef00f6c493884b168e0d85ade0b
Mateusz <Spaaw@poczta.fm>
2017-03-22 20:33:36 +0100

    Working hex to decimal converter.

--- boot/real_mode/hex_to_dec.asm
+++ boot/real_mode/hex_to_dec.asm
@@ -1,12 +1,40 @@
-;input: dx
+; ConvertNumber
+;   Input:
+;     ax = Number to be converted
+;     bx = Base
+;   
+;   Output:
+;     si = Start of NUL-terminated buffer
+;          containing the converted number
+;          in ASCII represention.
+
 hex_to_dec:
-	pusha
-	mov cx, 0; power
-	
-loop:
-	mov bx, dx
-	and bx, 0x000f
-	mov ax, 16
-	mul cx ; multiply power * 16
-	mov ax, bx
-	mul cx ; multiply digit * 16^power
+    push ax            ; Save modified registers
+    push bx
+    push dx
+    mov bx, 10
+    mov si, bufferend  ; Start at the end
+.convert:
+    xor dx, dx         ; Clear dx for division
+    div bx             ; Divide by base
+    add dl, '0'        ; Convert to printable char
+    cmp dl, '9'        ; Hex digit?
+    jbe .store         ; No. Store it
+    add dl, 'A'-'0'-10 ; Adjust hex digit
+.store:
+    dec si             ; Move back one position
+    mov [si], dl       ; Store converted digit
+    and ax, ax         ; Division result 0?
+    jnz .convert       ; No. Still digits to convert
+
+	mov bx, si
+	call print
+
+    pop dx             ; Restore modified registers	
+    pop bx
+    pop ax
+    ret
+    
+buffer: times 16 db 0
+bufferend:
+    db 0
