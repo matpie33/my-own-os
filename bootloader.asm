9ea940bbc15701d33b94c25e619f94bd23e395e9
mateusz <spaaw@poczta.fm>
2022-05-18 20:51:06 +0200

    Load external file.

--- bootloader.asm
+++ bootloader.asm
@@ -7,6 +7,20 @@ msg db "Welcome to my operating system", 0ah, 0dh, 0h
 boot:
 	cli
 	cld
+	
+	mov ax, 0x50
+	mov es, ax
+	xor bx, bx
+
+	mov al, 2
+	mov ch, 0 
+	mov cl, 2
+	mov dh, 0
+	mov dl, 0
+	mov ah, 0x02
+	int 0x13
+	jmp 0x50:0x0
+
 	hlt
 	
 times 510 - ($-$$) db 0
